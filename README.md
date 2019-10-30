4d16e6941f6a37595bfa07eaf388118ea074854e
Vili Hätönen <vili.hato@gmail.com>
2019-10-30 13:37:30 +0200

    stick to legacy nexus-prisma for compatability with prisma 1

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Prisma + PSQL + TS + GraphQL + Yoga + Nexus
 
 Following [Prisma docs](https://www.prisma.io/docs/get-started/01-setting-up-prisma-new-database-TYPESCRIPT-t002/)
+
+## See what's up
+
+`docker-compose up -d`
+
+`prisma init --endpoint http://localhost:4466`
+
+`prisma deploy`
+
+`npm run dev`
+
+go to `localhost:4000` to discover stuff with graphql
--- index.ts
+++ index.ts
@@ -1,30 +1,73 @@
 import { prisma } from './generated/prisma-client'
+import datamodelInfo from './generated/nexus-prisma'
+import * as path from 'path'
+import { stringArg, idArg } from 'nexus'
+import { prismaObjectType, makePrismaSchema } from 'nexus-prisma'
+import { GraphQLServer } from 'graphql-yoga'
 
-// A `main` function so that we can use async/await
-async function main() {
-  // Create a new user with a new post
-  const newUser = await prisma.createUser({
-    name: 'Bob',
-    email: 'bob@prisma.io',
-    posts: {
-      create: [
-        {
-          title: 'Join us for GraphQL Conf in 2019',
-        },
-        {
-          title: 'Subscribe to GraphQL Weekly for GraphQL news',
-        },
-      ],
-    },
-  })
-  console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
+const Query = prismaObjectType({
+  name: 'Query',
+  definition(t) {
+    t.prismaFields(['post'])
+    t.list.field('feed', {
+      type: 'Post',
+      resolve: (_, args, ctx) =>
+        ctx.prisma.posts({ where: { published: true } }),
+    })
+    t.list.field('postsByUser', {
+      type: 'Post',
+      args: { email: stringArg() },
+      resolve: (_, { email }, ctx) =>
+        ctx.prisma.posts({ where: { author: { email } } }),
+    })
+  },
+})
 
-  // Read all users from the database and print them to the console
-  const allUsers = await prisma.users()
-  console.log(allUsers)
+const Mutation = prismaObjectType({
+  name: 'Mutation',
+  definition(t) {
+    t.prismaFields(['createUser', 'deletePost'])
+    t.field('createDraft', {
+      type: 'Post',
+      args: {
+        title: stringArg(),
+        authorId: idArg({ nullable: true }),
+      },
+      resolve: (_, { title, authorId }, ctx) =>
+        ctx.prisma.createPost({
+          title,
+          author: { connect: { id: authorId } },
+        }),
+    })
+    t.field('publish', {
+      type: 'Post',
+      nullable: true,
+      args: { id: idArg() },
+      resolve: (_, { id }, ctx) =>
+        ctx.prisma.updatePost({
+          where: { id },
+          data: { published: true },
+        }),
+    })
+  },
+})
 
-  const allPosts = await prisma.posts()
-  console.log(allPosts)
-}
+const schema = makePrismaSchema({
+  types: [Query, Mutation],
 
-main().catch(e => console.error(e))
+  prisma: {
+    datamodelInfo,
+    client: prisma,
+  },
+
+  outputs: {
+    schema: path.join(__dirname, './generated/schema.graphql'),
+    typegen: path.join(__dirname, './generated/nexus.ts'),
+  },
+})
+
+const server = new GraphQLServer({
+  schema,
+  context: { prisma },
+})
+server.start(() => console.log('Server is running on http://localhost:4000'))
